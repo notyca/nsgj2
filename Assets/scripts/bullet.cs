6303ccbdca81f9ecbf6db90286a61ef50e9538d2
notyca <uliyoth@proton.me>
2024-12-28 02:37:10 -0500

    enemy1

---
+++ Assets/scripts/bullet.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("this it the thing");
+    }
+}
