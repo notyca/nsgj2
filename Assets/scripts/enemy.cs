385e8028b8a5e86e8ca0bd03542ddecc89477f03
notyca <uliyoth@proton.me>
2024-12-28 15:54:18 -0500

    enemy2

--- Assets/scripts/enemy.cs
+++ Assets/scripts/enemy.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 public class enemy : MonoBehaviour
 {
     [SerializeField] private GameObject bullet;
+    private float bullet_speed = 5.0f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        StartCoroutine(ShootBullets());
+        StartCoroutine(PowerUp());
     }
 
     // Update is called once per frame
@@ -17,15 +18,25 @@ public class enemy : MonoBehaviour
         
     }
 
+    IEnumerator PowerUp()
+    {
+        yield return new WaitForSeconds(2);
+        StartCoroutine(ShootBullets());
+    }
+
     IEnumerator ShootBullets()
     {
-        Vector2 direction = Vector2.up;
+        Vector2 direction = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.forward) * Vector2.up;
 
         while (true)
         {
-            GameObject new_bullet = Instantiate(bullet);
-            new_bullet.transform.position = transform.position;
-            new_bullet.GetComponent<Rigidbody2D>().linearVelocity = direction;
+            for (int i = 0; i < 3; ++i)
+            {
+                GameObject new_bullet = Instantiate(bullet);
+                new_bullet.transform.position = transform.position;
+                new_bullet.GetComponent<Rigidbody2D>().linearVelocity = direction * bullet_speed;
+                direction = Quaternion.AngleAxis(120, Vector3.forward) * direction;
+            }
             direction = Quaternion.AngleAxis(20, Vector3.forward) * direction;
             yield return new WaitForSeconds(1f);
         }
